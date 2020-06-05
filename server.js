f3f4024a0fc7b83cbff01835853d39a50a8f2111
masoodmjan <mjan@mazataz.com>
2020-06-05 12:41:36 +0100

    update server code

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var port = 4000
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.end('Welcome askmas devops course 1\n')
+  response.end('Welcome askmas devops course 1\n Updated')
 })
 
 server.listen(port)
